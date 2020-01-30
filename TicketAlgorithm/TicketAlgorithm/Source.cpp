002c4ad46edbdad55e1865586707a6c2dad57267
Eoin <c002217717@itcarlow.ie>
2020-01-30 14:56:19 +0000

    algorithm

--- TicketAlgorithm/TicketAlgorithm/Source.cpp
+++ TicketAlgorithm/TicketAlgorithm/Source.cpp
@@ -3,77 +3,87 @@
 #include <chrono>
 #include <mutex>
 
+#include <SFML/Graphics.hpp>
+
 #include <iostream>
 #include <string>
 #include <array>
 
 
 
-static const int n = 5;
-int number, next = 1;
-std::array<int, n> turn;
+static const int g_N = 5;
+int g_number, g_next = 1;
+std::array<int, g_N> g_turn;
 
-int numOfThreads = 0;
-std::mutex mtx;
+int g_numOfThreads = 0;
+std::mutex g_mtx;
+
+void criticalSection();
 
 void ticketAlgorithm()
 {
-	int i = numOfThreads++;
+	int i = g_numOfThreads++;
 
-	mtx.lock();
+	g_mtx.lock();
 	std::cout << "Thread" << i << std::endl;
-	mtx.unlock();
+	g_mtx.unlock();
 
 	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
 	while (true)
 	{
-		turn[i] = number;
-		number++;
+		g_turn[i] = g_number;
+		g_number++;
 
-		mtx.lock();
-		std::cout << "t" << i << "\t Turn: " << turn[i] << std::endl;
-		mtx.unlock();
+		g_mtx.lock();
+		std::cout << "t" << i << "\t Turn: " << g_turn[i] << std::endl;
+		g_mtx.unlock();
 
 		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
-		while(turn[i] != next)
+		while(g_turn[i] != g_next)
 		{
 			continue;
 		}
 
-		mtx.lock();
-		std::cout << "t" << i << "\t +Critical Section " << std::endl;
-		mtx.unlock();
+		g_mtx.lock();
+		std::cout << "t" << i << "\t  + Critical Section " << std::endl;
+		g_mtx.unlock();
 
 		std::cout << "Critical Section" + i << std::endl;
+		criticalSection();
 
-		next++;
+		g_next++;
 
-		mtx.lock();
-		std::cout << "t" << i << "\t Next: " << next << std::endl;	
-		mtx.unlock();
+		g_mtx.lock();
+		std::cout << "t" << i << "\t Next: " << g_next << std::endl;	
+		g_mtx.unlock();
 
 	}
 }
 
 int main(void)
 {
-	for (int i = 0; i < turn.size(); i++)
+	for (int i = 0; i < g_turn.size(); i++)
 	{
-		turn[i] = 0;
+		g_turn[i] = 0;
 	}
 
 	std::thread ticket1(ticketAlgorithm);
 	std::thread ticket2(ticketAlgorithm);
+	std::thread ticket3(ticketAlgorithm);
+	std::thread ticket4(ticketAlgorithm);
 	
 	ticket1.join();
 	ticket2.join();
+	ticket3.join();
+	ticket4.join();
 	
 	std::cin.get();
 	return 0;
 }
 
-
-
-
+void criticalSection()
+{
+	
+}
